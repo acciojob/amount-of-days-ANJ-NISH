3d634ccd38f0cfade77362ecc6255a5ab63263c3
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-11-14 22:54:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function daysOfAYear(year)
+{
+	if(year%4!==0)
+	{
+		return 365;
+	}
+	else if(year%4===0 && year%400!==0)
+	{
+		return 365;
+	}
+	else
+	{
+		return 366;
+	}
+}
